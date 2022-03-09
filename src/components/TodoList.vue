9e48a7ae2f71ee354f576bfb2fdbbbbd569a1731
Alinde <alinde.m@hotmail.com>
2022-03-09 09:06:18 +0100

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+
+// give each todo a unique id
+let id = 0
+
+const newTodo = ref('')
+const todos = ref([
+
+])
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
+
+
+
+</script>
+
+<template>
+  <input v-model="newTodo" @keyup.enter="addTodo">
+  <button @click="addTodo">Add Todo</button>
+  <ul>
+    <li  @click="e => e.target.classList.toggle('done')" class="" v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      
+      
+      
+      <button @click="removeTodo(todo)">X</button>
+      
+    </li>
+  </ul>
+
+  
+</template>
+ 
+ <style>
+
+
+.done {
+  text-decoration: line-through;
+  
+}
+
+ </style>
